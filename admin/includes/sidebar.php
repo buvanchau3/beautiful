c9a3778029a2c6811a1cbd13b2ef9cd0cecaf60b
unknown <you@example.com>
2024-11-10 15:07:23 +0700

    first commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,233 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100%; height: 80px; padding: 0 100px;">
+        <div class="image-logo" style="margin-right: 30px;">
+           <a href="../html/index.php"> <img src="#" alt="logo"></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+                <li class="nav-item active">
+                    <a class="nav-link view-nav" href="../html/newproduct.php">SẢM PHẨM MỚI</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link view-nav dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        NAM
+                    </a>
+                    <div class="dropdown-menu p-3" style="width: 600px;">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item"
+                                            style="color: black; font-size: 18px; font-weight: 600;" href="#">Danh mục
+                                            sản phẩm</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo phông/ Áo thun</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo nỉ & Áo Hoodie</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo khoác</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo/ Quần giữ nhiệt</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo len </a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo mặc nhà/ Đồ ngủ</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item" href="#"></a></li>
+                                    <li><a class="dropdown-item" href="#">Quần dài & Quần Jean</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo thể thao</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo polo</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo sơ mi</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo chông nắng </a></li>
+                                    <li><a class="dropdown-item" href="#">Đồ lót</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <a href="#"> <img style="width: 100%; height: 200px" src="../image/đồ-nam-1.png"
+                                            alt="anh-mau"></a>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <a href="#"> <img style="width: 100%; height: 200px " src="../image/đồ-nam-2.png"
+                                            alt="anh-mau"></a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link view-nav dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        NỮ
+                    </a>
+                    <div class="dropdown-menu p-3" style="width: 600px;">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item"
+                                            style="color: black; font-size: 18px; font-weight: 600;" href="#">Danh mục
+                                            sản phẩm</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo phông/ Áo thun</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo nỉ & Áo Hoodie</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo khoác</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo/ Quần giữ nhiệt</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo len </a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo mặc nhà/ Đồ ngủ</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item" href="#">Váy</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần dài & Quần Jean</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo thể thao</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo polo</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo sơ mi</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo chông nắng </a></li>
+                                    <li><a class="dropdown-item" href="#">Đồ lót</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <a href="#"> <img style="width: 100%; height: 200px" src="../image/đồ-nữ-1.png"
+                                            alt="anh-mau"></a>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <a href="#"> <img style="width: 100%; height: 200px " src="../image/đồ-nữ-2.png"
+                                            alt="anh-mau"></a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link view-nav dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        BÉ GÁI
+                    </a>
+                    <div class="dropdown-menu p-3" style="width: 600px;">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item"
+                                            style="color: black; font-size: 18px; font-weight: 600;" href="#">Danh mục
+                                            sản phẩm</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo phông/ Áo thun</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo nỉ & Áo Hoodie</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo khoác</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo/ Quần giữ nhiệt</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo len </a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo mặc nhà/ Đồ ngủ</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item" href="#">Váy</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần dài & Quần Jean</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo thể thao</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo polo</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo sơ mi</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo chông nắng </a></li>
+                                    <li><a class="dropdown-item" href="#">Đồ lót</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <a href="#"> <img style="width: 100%; height: 200px" src="../image/bé-gai-1.png"
+                                            alt="anh-mau"></a>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <a href="#"> <img style="width: 100%; height: 200px " src="../image/bé-gái-2.png"
+                                            alt="anh-mau"></a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link view-nav dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        BÉ TRAI
+                    </a> 
+                    <div class="dropdown-menu p-3" style="width: 600px;">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item"
+                                            style="color: black; font-size: 18px; font-weight: 600;" href="#">Danh mục
+                                            sản phẩm</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo phông/ Áo thun</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo nỉ & Áo Hoodie</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo khoác</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo/ Quần giữ nhiệt</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo len </a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo mặc nhà/ Đồ ngủ</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <li><a class="dropdown-item" href="#"></a></li>
+                                    <li><a class="dropdown-item" href="#">Quần dài & Quần Jean</a></li>
+                                    <li><a class="dropdown-item" href="#">Quần áo thể thao</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo polo</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Áo sơ mi</a></li>
+                                    <li><a class="dropdown-item" href="#">Áo chông nắng </a></li>
+                                    <li><a class="dropdown-item" href="#">Đồ lót</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <ul class="list-unstyled">
+                                    <a href="#"> <img style="width: 100%; height: 200px" src="../image/bé-trai-1.png"
+                                            alt="anh-mau"></a>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <a href="#"> <img style="width: 100%; height: 200px" src="../image/bé-trai-2.png"
+                                            alt="anh-mau"></a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <form class="d-flex" style="margin-right: 30px;">
+                <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+        <ul class="navbar-nav text-center" style="width: 300px">
+            <li class="nav-item" style="font-size: 15px;">
+                <a class="nav-link" style="width:120px" href="#"><i style="padding-right:5px;" class="bi bi-shop"></i>Cửa hàng</a>
+            </li>
+            <li class="nav-item" style="ont-size: 15px;">
+                <a class="nav-link" style="width:120px" href="../include/account.php"><i style="padding-right:5px;" class="bi bi-people"></i>Tài khoản</a>
+            </li>
+            <li class="nav-item" style="ont-size: 15px;">
+                <a class="nav-link" style="width:120px" href="../html/cart.php"><i style="padding-right:5px;" class="bi bi-cart4"></i>Giỏ hàng</a>
+            </li>
+        </ul>
+    </nav>
